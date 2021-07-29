a0a5e15898a316ecb98222784b0d0162571abdfc
khadkeshwarkp <kunalkhadkeshwar21@gmail.com>
2021-07-29 12:46:38 +0530

    jh

--- demoJenkins/Program.cs
+++ demoJenkins/Program.cs
@@ -6,7 +6,7 @@ namespace demoJenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!")
+            Console.WriteLine("Hello World!");
         }
     }
 }
